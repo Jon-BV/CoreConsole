8b166aa6efdcaf769e07457dacab625ad57527a9
vs-test01 <vs-test01@DESKTOP-5RK059A>
2020-10-13 00:18:44 +0400

    Modified

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CoreConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Jon");
         }
     }
 }
